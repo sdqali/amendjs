a04047e61a931b7d9a09a176d1e45417abd5029c
Sadique Ali <sadiqalikm@gmail.com>
2013-10-28 18:00:21 -0700

    equalIgnoreCase.

--- src/amend.js
+++ src/amend.js
@@ -1,5 +1,8 @@
 Amend = {
     capitalize: function(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    equalIgnoreCase: function(thisString, thatString) {
+	return thisString.toUpperCase() === thatString.toUpperCase();
     }
 }
--- tests/spec/amend_spec.js
+++ tests/spec/amend_spec.js
@@ -12,4 +12,22 @@ describe("ImageFitter", function() {
 	    expect(Amend.capitalize("foo bar is awesome")).toEqual("Foo bar is awesome");
 	});
     });
+
+    describe(".equalIgnoreCase", function() {
+	it("can compare single characters", function() {
+	    expect(Amend.equalIgnoreCase("f", "F")).toBe(true);
+	});
+
+	it("can compare single words", function() {
+	    expect(Amend.equalIgnoreCase("foo", "FoO")).toBe(true);
+	});
+
+	it("can compare string with multiple words", function() {
+	    expect(Amend.equalIgnoreCase("foo bAr iS Bar", "FOO BaR IS BAr")).toBe(true);
+	});
+
+	it("can compare string with symbols words", function() {
+	    expect(Amend.equalIgnoreCase("foo bAr iS %#@ B-a*r", "FOO BaR IS %#@ B-A*r")).toBe(true);
+	});
+    });
 });
